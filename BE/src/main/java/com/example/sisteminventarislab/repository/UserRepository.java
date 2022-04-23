01eb75ef3e40c03cedebf81a3ac553dee804f188
Joshua Erlangga Sakti <joshua.sakti@gdn-commerce.com>
2022-04-23 16:17:17 +0700

    moved to new project files for BE

--- BE/src/main/java/com/example/sisteminventarislab/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sisteminventarislab.repository;
-
-import com.example.sisteminventarislab.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
