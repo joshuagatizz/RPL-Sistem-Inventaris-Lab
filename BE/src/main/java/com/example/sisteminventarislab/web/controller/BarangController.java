6124e9a56f0add0586c18d6f336b2d08e218d410
Butrahandisya <butrahandisya@gdn-commerce.com>
2022-05-21 12:58:39 +0700

    menambahkan dokumentasi untuk webservice update dan delete barang (pada level controller dan service)

--- BE/src/main/java/com/example/sisteminventarislab/web/controller/BarangController.java
+++ BE/src/main/java/com/example/sisteminventarislab/web/controller/BarangController.java
@@ -37,6 +37,20 @@ public class BarangController {
         return barangService.getAllBarang();
     }
 
+    /**
+     * Method updateBarang pada class controller BarangController ini
+     * menerima id dari barang yang akan diperbaharui, dan request berupa
+     * detail dari barang yang mana akan dioverwrite pada barang tersebut.
+     *
+     * Method updateBarang pada controller ini akan memanfaatkan
+     * implementasi dari method updateBarang pada class interface
+     * service BarangService, yang mana diimplementasi pada class
+     * BarangServiceImpl.
+     *
+     * @param id, yaitu id dari barang yang akan diperbaharui
+     * @param request, detail dari barang yang akan dioverwrite
+     * @return Barang, yaitu hasil dari perbaharuan barang yang dilakukan
+     */
     @ApiOperation("update Barang by id")
     @PutMapping(
             path = "/{id}",
@@ -45,6 +59,18 @@ public class BarangController {
         return barangService.updateBarang(id, request);
     }
 
+    /**
+     * Method deleteBarang pada class controller BarangController ini menerima id
+     * dari barang yang akan dihapus.
+     *
+     * Method updateBarang pada controller ini akan memanfaatkan
+     * implementasi dari method deleteBarang pada class interface
+     * service BarangService, yang mana diimplementasi pada class
+     * BarangServiceImpl.
+     *
+     * @param id, yaitu id dari barang yang akan dihapus.
+     * @return nilai boolean, yang menunjukkan keberhasilan dari penghapusan barang.
+     */
     @ApiOperation("delete Barang by id")
     @DeleteMapping(path = "/{id}")
     public boolean deleteBarang(@PathVariable String id) {
